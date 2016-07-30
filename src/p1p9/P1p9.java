ce99371e57841ba85028c2d67d7fa8cf57136962
Edwin <edwinacubillos@gmail.com>
2016-07-29 20:59:21 -0500

    Punto 9 practica 1

---
+++ src/p1p9/P1p9.java
@@ -0,0 +1,24 @@
+/*
+ *  Generar los números del 1 al 10 utilizando un ciclo que 
+vaya de 10 a 1. 
+ */
+package p1p9;
+
+/**
+ *
+ * @author Edwin
+ */
+public class P1p9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero;
+        for (int i=10; i>0; i--){
+            numero = 11-i;
+            System.out.println(numero);
+        }
+    }
+    
+}
